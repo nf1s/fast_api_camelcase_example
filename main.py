614c1cd85b3279dba17a56890a9b2cf699454201
ahmednafies <ahmed.nafies@gmail.com>
2020-01-30 15:51:47 +0100

    refactores

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from models import User
 
 
 app = FastAPI()
--- models.py
+++ models.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
+from humps import camelize
+
+
+def to_camel(string):
+    return camelize(string)
 
 
 class User(BaseModel):
     first_name: str
     last_name: str = None
     age: float
+
+    class Config:
+        alias_generator = to_camel
